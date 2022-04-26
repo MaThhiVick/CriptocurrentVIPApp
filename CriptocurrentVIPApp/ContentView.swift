6b13dbd143d78b354788b4db078e1cd09be5715c
MaThhiVick <matheusav@outlook.com.br>
2022-04-26 16:54:41 -0300

    Initial Commit

---
+++ CriptocurrentVIPApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CriptocurrentVIPApp
+//
+//  Created by Matheus Vicente on 26/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CriptocurrentVIPApp/CriptocurrentVIPAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CriptocurrentVIPAppApp.swift
+//  CriptocurrentVIPApp
+//
+//  Created by Matheus Vicente on 26/04/22.
+//
+
+import SwiftUI
+
+@main
+struct CriptocurrentVIPAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
