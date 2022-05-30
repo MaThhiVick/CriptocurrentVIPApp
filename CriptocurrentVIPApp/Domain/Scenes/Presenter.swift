74205930bbc6acc2ef1ea9ae8ed9ea413983126e
MaThhiVick <matheusav@outlook.com.br>
2022-05-30 16:11:41 -0300

    Making second view works with User Default

--- CriptocurrentVIPApp/Domain/Scenes/Presenter.swift
+++ CriptocurrentVIPApp/Domain/Scenes/Presenter.swift
@@ -5,20 +5,20 @@
 //  Created by Matheus Vicente on 10/05/22.
 //
 
-//import Foundation
-//protocol CoinSelectedPresentation {
-//    func presentCoin(response:
-//                     CoinSelected.LoadCoin.Response)
-//}
-//
-//class PresenterCoinSelected {
-//    var view: CoinSelectedDiplayLogic?
-//}
-//
-//extension PresenterCoinSelected: CoinSelectedPresentation {
-//    
-//
-//func presentCoin(response: CoinSelected.LoadCoin.Response) {
-//    
-//}
-//}
+import Foundation
+protocol CoinSelectedPresentation {
+    func presentCoin(response:
+                     CoinSelected.LoadCoin.Response)
+}
+
+class PresenterCoinSelected {
+    var view: CoinSelectedDiplayLogic?
+}
+
+extension PresenterCoinSelected: CoinSelectedPresentation {
+    
+    func presentCoin(response: CoinSelected.LoadCoin.Response) {
+        let viewModel = CoinSelected.LoadCoin.ViewModel.init(coinBought: response.coinLoaded)
+        self.view?.displayCoinData(viewModel: viewModel)
+    }
+}
--- CriptocurrentVIPApp/Domain/Scenes/Worker.swift
+++ CriptocurrentVIPApp/Domain/Scenes/Worker.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+class SaveLoadCoinWorker {
+    private let defaults = UserDefaults.standard
+
+    func save(coin: InformationBoughtCoin) {
+        var listOfCoin = load() //caso nao tenha nada e fazer um append
+        listOfCoin.append(coin)
+        let encoder = JSONEncoder()
+        if let encoded = try? encoder.encode(listOfCoin) {
+            defaults.set(encoded, forKey: "ListOfCoinsBought")
+
+        }
+    }
+    
+    func load() -> [InformationBoughtCoin] {
+        var valueReturn: [InformationBoughtCoin]?
+        if let listOfCoinsBought = defaults.object(forKey: "ListOfCoinsBought") as? Data {
+            let decoder = JSONDecoder()
+            if let coinList = try? decoder.decode([InformationBoughtCoin].self, from: listOfCoinsBought) {
+                valueReturn = coinList
+            }
+        }
+        
+        if let valueReturn = valueReturn {
+            return valueReturn
+        } else {
+            return []
+        }
+    }
+}
